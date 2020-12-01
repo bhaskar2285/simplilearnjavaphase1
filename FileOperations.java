654a13aa73569a18234681549727cb6c481ada1c
bhaskar2285 <75271773+bhaskar2285@users.noreply.github.com>
2020-12-02 01:45:52 +0530

    Add files via upload

--- FileOperations.java
+++ FileOperations.java
@@ -1,95 +1,118 @@
 package lockedMe.com;
 import java.util.Scanner;
-
-
 import java.io.*;
 
 
 
 public class FileOperations {
-	public void handleFiles() throws Exception, IOException{
-	boolean userflag = true;
+	public void handleFiles() throws Exception{
+	// add class Exception to check for any exception thrown by the class
+	
+	//try catch block to handle any exception that occur during option selection
+    boolean userflag = true;
 	while (userflag){
-		
 	
 	try{
-	System.out.println("1. Add user mentioned files");
-    System.out.println("2. Delete user mentioned files");
-    System.out.println("3. Search user mentioned files");
-    System.out.println("Any key to previous menu");
-	String option2;
-    
-    Scanner opt2 = new Scanner(System.in);
-    option2 = opt2.next();
-    
 
-    
-	switch(option2){
-	case "1":
-		addFile addfile = new addFile();
-		addfile.fileadd();
-		continueflow2 cf1 = new continueflow2();
-		userflag = cf1.continuetransaction();
-		//continue;
-		break;
-	case "2":
-		deleteFile delfile = new deleteFile();
-		delfile.fileDelete();
-		continueflow2 cf2 = new continueflow2();
-		userflag = cf2.continuetransaction();
-		//continue;
-		break;
-	case "3":
-		SearchFile searchit = new SearchFile();
-		searchit.search();
-		continueflow2 cf3 = new continueflow2();
-		userflag = cf3.continuetransaction();
-		//continue;
-		break;
-	default:
-		userflag = false;
-		//continue;
-		break;
-	
-	}
-	//return true;
-	}catch(Exception e){
-		System.out.println("invalid input");
-		//userflag = false;
-	}
+		System.out.println("Welcome to File Handling Menu, Please select from the options below:");
+	    System.out.println("1. Add user mentioned files");
+        System.out.println("2. Delete user mentioned files");
+        System.out.println("3. Search user mentioned files");
+        System.out.println("Press any other key to go to previous menu");
+	    
+        String option2;
+        
+        // take input from user to select the desired file operation
+        Scanner opt2 = new Scanner(System.in);
+        option2 = opt2.next();
+        
+        //opt2.close();
+            
+	    switch(option2){
+	        
+	        case "1":
+	        	//use addfile class to add file to user mentioned directory
+	        	addFile addfile = new addFile();
+	        	addfile.fileadd();
+	        	continueflow2 cf1 = new continueflow2();
+	        	userflag = cf1.continuetransaction();
+	        	break;
+	        
+	        case "2":
+	        	//use deletefile class to delete file from user mentioned directory
+	        	deleteFile delfile = new deleteFile();
+	        	delfile.fileDelete();
+	        	continueflow2 cf2 = new continueflow2();
+	        	userflag = cf2.continuetransaction();
+	        	break;
+	        
+	        case "3":
+	        	//use searchfile class to search file from user mentioned directory
+	        	SearchFile searchit = new SearchFile();
+	        	searchit.search();
+	        	continueflow2 cf3 = new continueflow2();
+	        	userflag = cf3.continuetransaction();
+	        	break;
+	        
+	        default:
+	        	userflag = false;
+	        	break;
+	        
+	        }
+	    }catch(Exception e){
+	    	    
+	    	    System.out.println("invalid input");
+	    }
     }
 	
 	}
 }
-
+// filenamefilter interface class to use call the accept method inorder to search a user mentioned file
 class findMyFile implements FilenameFilter{
+	
 	String fileToSearch;
+	//initialize constructor with file name and location of the file to search
 	public findMyFile(File location,String fileToSearch){
 		
 		this.fileToSearch = fileToSearch;
 		
 	}
+	//accept method to search the file in the user mentioned location
 	public boolean accept(File location, String name ){
 		return name.startsWith(fileToSearch);
 		
 	}
 }
+//Search file class is used to search a user mentioned file in the user mentioned directory
+
 class SearchFile{
 	public void search(){
+		
 		System.out.println("Enter name of the directory you want to search files in the"
 				+ " format C://Users//bhaskar//Desktop//");
+		//take input from user for the pathname of the directory
 		Scanner S = new Scanner(System.in);
 		String pathnames;
-		pathnames = S.next();
+		pathnames = S.nextLine();
+		//S.close();
+		
+		//use file class constructor to initialize it with the pathname
 		File location = new File(pathnames);
-		//File [] files = location.listFiles();
-		System.out.println("Enter name of the file to be searched");
-		@SuppressWarnings("resource")
+		
+		System.out.println("Enter name of the file to be searched:");
+		
+		//Take input from user reqgarding the filename
+		
+		
 		Scanner filesrch = new Scanner(System.in);
 		String srchfile;
-		srchfile = filesrch.next();
+		srchfile = filesrch.nextLine();
+		//filesrch.close();
+		
+		//use findmyfile class to search the file 
 		findMyFile srch = new findMyFile(location, srchfile);
 		
+		//load the flist array with files found by the names entered by user
 		String[] flist = location.list(srch);
 			
 		if (flist == null){
@@ -102,20 +125,32 @@ class SearchFile{
 			}
 			else{
 			for(int i=0;i<flist.length;i++){
+				// display the files found to the user
 				System.out.println("We have found the file you mentioned: "+ flist[i]);
 			}
 			}
 		}
+
 	}
 }
+
+//user delete file class to delete files from a directory
 class deleteFile{
 	public void fileDelete(){
+		
 		System.out.println("Enter the full path of the file to be deleted in "
 				+ "the format C://Users//bhaskar//Desktop//Filename.txt");
+		
+		// take input from the user for the filelocation and file the user need to delete
+		
 		Scanner S = new Scanner(System.in);
 		String pathnames;
-		pathnames = S.next();
+		pathnames = S.nextLine();
+		//S.close();
+		
+		//use file class to call the delete method to delete the file
 		File location = new File(pathnames);
+		
 		if (location.delete()){
 			System.out.println("Delete Successfull");
 		}
@@ -125,24 +160,20 @@ class deleteFile{
 		
 	}
 }
+
+// add class to add file to the user mentioned directory
 class addFile{
 	public void fileadd() throws IOException,SecurityException{
 		System.out.println("Enter the full path of the file to be added "
 				+ "in the format C://Users//bhaskar//Desktop//Filename.txt");
+		
 		Scanner S = new Scanner(System.in);
 		String pathnames;
-		pathnames = S.next();
+		pathnames = S.nextLine();
+		//S.close();
+		
 		File location = new File(pathnames);
-	
-		/*try{
-			FileWriter location = new FileWriter(pathnames);
-			location.write("hi");
-			location.close();
-			
-		}catch(IOException ioe){
-			System.out.println("Could not create file");
-			ioe.printStackTrace();
-		}*/
+	    
 		if ( location.createNewFile() ){
 			System.out.println("File Created successfully");
 		}
@@ -150,20 +181,29 @@ class addFile{
 		{
 			System.out.println("Could not create file");
 		}
+        
 		
 	}
 }
 
+//abstract class to add logic to continue the go back forth across menu
+
 abstract class continueflow{
 	abstract public boolean continuetransaction();
 }
+
+//continueflow class to continue option selection after one user operation is completed
+
 class continueflow2 extends continueflow{
     public boolean continuetransaction(){
-    String ip1;
-    
-	System.out.println("Press Any key to continue");
-    Scanner input1 = new Scanner(System.in);
-    ip1 = input1.next();
-    return true;
+        
+        
+	    System.out.println("Press Any key to continue");
+        Scanner input1 = new Scanner(System.in);
+        input1.next();
+        
+        //input1.close();
+        
+        return true;
     }
 }
--- directoryDisplay.java
+++ directoryDisplay.java
@@ -1,15 +1,23 @@
 package lockedMe.com;
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.text.SimpleDateFormat;
 
 
 public class directoryDisplay {
 	public static void main(String [] args) throws IOException{
+		
+		// added IOException to main to handle the IOException raised in runtime
 		boolean userflag = true;
-	    System.out.println("#################################################");
+	    
+		
+		System.out.println("#################################################");
 	    System.out.println("#                                               #");
 	    System.out.println("#                  LockedMe.com                 #");
 	    System.out.println("#              Developer-Bhaskar Singh          #");
@@ -19,140 +27,196 @@ public class directoryDisplay {
 	    System.out.println("#  delete and search the files they want.       #");
 	    System.out.println("#################################################");
 	    
+	    // create object cf1 for to continue the flow to below menu from other menu's
 		continueflow4 cf1 = new continueflow4();
 		userflag = cf1.continuetransaction();
 		
 		while (userflag){
 
-		System.out.println("Select your options from the menu below");
-		System.out.println("1. Display files in directory in ascending order");
-		System.out.println("2. Add/Delete/Search mentioned files");
-		System.out.println("3. Exit App");
-		
-		String option;
-		
-		try{
-			Scanner opt = new Scanner(System.in);
-		    option = opt.next();
-		
-		
-		
-		
-		switch (option){
-		case "1":
-			
-			directoryDisplayAscending asc = new directoryDisplayAscending();
-			asc.DisplayAscending();
-			continueflow3 cf = new continueflow3();
-			userflag = cf.continuetransaction();
-			break;
-			
-		case "2": 
-		    {   FileOperations fileh = new FileOperations();
-	            try{
-	            	//userflag = fileh.handleFiles();
-	            	fileh.handleFiles();
-	            }catch(Exception E){
-	            	System.out.println("Continuing with some issues in file handling");
-	            	userflag = true;
-	            }
-	            //userflag = continuetransaction();
-	            break;
-		    }
-
-		case "3":
-			System.out.println("Good Bye, Thank you for using our app");
-			userflag = false;
-			break;
-	    default:
-	    	System.out.println("Please Select the correct option 1, 2 or 3");
-			//userflag = false;
-			break;
-		
-			
-			
-		
-
-		}
-
-	    
-		}catch(Exception e){
-			System.out.println("Invalid input");
-			//e.printStackTrace();
-		}	
+		      System.out.println("Please select from the options in the menu below:");
+		      System.out.println("1. Display files in directory in ascending order");
+		      System.out.println("2. Add/Delete/Search mentioned files");
+		      System.out.println("3. Exit App");
+		      
+		      String option;
+		      
+		      try{ 
+		    	  
+		    	  // use try catch block to handle any invalid input from user
+		          // user scanner class to take the input from user for select the option
+		          Scanner opt = new Scanner(System.in);
+		          option = opt.next();
+		          //opt.close();
+		          
+		          switch (option){
+		          case "1":
+		         
+		          	    // use the directory display class to display the contents in the directory sorted in ascending
+		          	    directoryDisplayAscending asc = new directoryDisplayAscending();
+		          	    asc.DisplayAscending();
+		          	    
+		          	    
+		          	    //use the continueflow3 class to ask user for continuing the flow or quit
+		          	    // userflag is used to control the flow to display menu:if userflag is false then 
+		          	    //while loop is discontinued and app closes else it continues.
+		          	    
+		          	    continueflow3 cf = new continueflow3();
+		          	    userflag = cf.continuetransaction();
+		          	    break;
+		          	
+		          case "2": 
+		              {   // use the FilOperations class to perform the Fileoperations like add,delete and Search
+		              	FileOperations fileh = new FileOperations();
+	                      try{
+	                      
+	                    	//use try catch block to handle any exception related to file handling methods
+	                      	fileh.handleFiles();
+	                      }catch(Exception E){
+	                      	
+	                    	System.out.println("Continuing with some issues in file handling");
+	                      	userflag = true;
+	                      
+	                      }
+	                     
+	                      break;
+		              }
+                  
+		          case "3":// to close app by making the userflag false
+		          	    System.out.println("Good Bye, Thank you for using our app");
+		          	    userflag = false;
+		          	    break;
+	              
+		          default:
+	              	    System.out.println("Please Select the correct option 1, 2 or 3");
+		          	    //userflag = false;
+		          	    break;
+		                
+		          }
+                  
+		      }catch(Exception e){
+		      	       //catch exception for any invalid input entered by the user
+		      	       System.out.println("Invalid input");
+		      	       
+		      	       //e.printStackTrace();
+		      }
 		}
 	
 	}
-
-    
-    
-
 }
 
-
+//directory display class to display directory contents sorted in ascending order
 class  directoryDisplayAscending{
 	public void DisplayAscending () {
-	System.out.println("Enter name of the directory in format (C://Users//bhaskar//Desktop//)");
-	Scanner S = new Scanner(System.in);
-	String  pathnames; 
-	pathnames = S.next();
-	
-	SimpleDateFormat dateformat = new SimpleDateFormat("MM/dd/YYYY h:mm:ss a");
-	long sizeoffile = 0l;
-	try{
-	File location = new File(pathnames);
-	
-	File [] files = location.listFiles();
-	 Arrays.sort(files);
-	 System.out.println("---------------------------------------------------------------------------------------");
-	 System.out.println(" " + "Filename               " + " | " + "Last-Modified               " + " | " + "Size");  
-	 System.out.println("---------------------------------------------------------------------------------------");
-	 for(File f : files){
-		 sizeoffile = f.length();
-	     //System.out.println(" " + f.getAbsoluteFile() + "| " + dateformat.format(f.lastModified()) + "| " + sizeoffile + " Bytes");
-		 System.out.println(" " + f.getName() + "| " + dateformat.format(f.lastModified()) + "| " + sizeoffile + " Bytes");
-	 }
-	}catch(Exception e){
-		System.out.println("Directory not found ");
-		
-	}
+	    
+		System.out.println("Enter name of the directory in format (C://Users//bhaskar//Desktop//)");
+	    
+	    // Take input from user for the directory name the user wants to look the contents of
+	    Scanner S = new Scanner(System.in);
+	    String  pathnames; 
+	    
+	    pathnames = S.nextLine();
+	    
+	    //S.close();
+	    
+	    // format the date and time using the SimpleDateFormat class
+	    SimpleDateFormat dateformat = new SimpleDateFormat("MM/dd/YYYY h:mm:ss a");
+	    
+	    long sizeoffile = 0l;
+	    
+	    try{
+	        // use Try catch block for any exception raised
+	        // Use File class and pass pathnames to constructor for initialization
+	         File location = new File(pathnames);
+	        
+	         //used files array to get the list of files from listfiles method and fill up the files array
+	         File [] files = location.listFiles();
+	        // use the Arrays.sort method to sort the files in ascending order
+             
+	         Arrays.sort(files);
+	         
+	         System.out.println("---------------------------------------------------------------------------------------");
+	         System.out.println(" " + "Filename               " + " | " + "Last-Modified               " + " | " + "Size");  
+	         System.out.println("---------------------------------------------------------------------------------------");
+	         
+	         //Use for loop to display the contents of the directory showing details like name,last modifed & file size.
+
+	         for(File f:files){
+	        	 
+	        	 
+	        	 sizeoffile = f.length();
+	             
+	        	 //System.out.println(" " + f.getAbsoluteFile() + "| " + dateformat.format(f.lastModified()) + "| " + sizeoffile + " Bytes");
+	        	 System.out.println(" " + f.getName() + "| " + dateformat.format(f.lastModified()) + "| " + sizeoffile + " Bytes");
+	             
+	        	 
+	         }
+	    }catch(Exception e){
+	    	  System.out.println("Directory not found ");
+	    	  //e.printStackTrace();
+	    	  
+	    	
+	    }
     
 	}
 }
 
 
+//continueflow3 class inherits the mehods of abstract class continueflow
+// this class is used when user have already finished to display directory contents
 class continueflow3 extends  continueflow {
     public boolean continuetransaction(){
-    String ip1;
-    
-	System.out.println("Do you want to continue? Press Yes (Y) to continue,Any other key to quit");
-    Scanner input1 = new Scanner(System.in);
-    ip1 = input1.next();
-    if (ip1.equals("Y") || ip1.equals("y") ){
-    	return  true;
-    }
-    else
-    {
-    	System.out.println("Good Bye, Thank you for using our app");
-    	return false;
-    }
+        
+    	String ip1;
+        
+	    System.out.println("Do you want to continue? Press Yes (Y) to continue,Any other key to quit");
+        try{
+	    Scanner input1 = new Scanner(System.in);
+        ip1 = input1.next();
+        //input1.close();
+        
+        if (ip1.equals("Y") || ip1.equals("y") ){
+        	return  true;
+        }
+        else
+        {
+        	System.out.println("Good Bye, Thank you for using our app");
+        	return false;
+        }
+        }catch(Exception e){
+			
+			System.out.println("invalid input");
+			return true;
+		}
     }
 }
 
+//continueflow4 class inherits the mehods of abstract class continueflow
+//this class is used at the start of the application and enter the menu for file handling
 class continueflow4 extends  continueflow {
     public boolean continuetransaction(){
-    String ip1;
-    
-	System.out.println(" Press Yes (Y) to Enter ,Any other key to quit");
-    Scanner input1 = new Scanner(System.in);
-    ip1 = input1.next();
-    if (ip1.equals("Y") || ip1.equals("y") ){
-    	return  true;
-    }
-    else
-    {
-    	System.out.println("Good Bye, Thank you for using our app");
-    	return false;
-    }
+        
+    	String ip1;
+        
+	    System.out.println(" Press Yes (Y) to Enter ,Any other key to quit");
+        try{
+        	
+	    Scanner input1 = new Scanner(System.in);
+        ip1 = input1.next();
+        //input1.close();
+        
+        if (ip1.equals("Y") || ip1.equals("y") ){
+        	return  true;
+        }
+        else
+        {
+        	System.out.println("Good Bye, Thank you for using our app");
+        	return false;
+        }
+        
+        }catch(Exception e){
+			
+			System.out.println("invalid input");
+			return true;
+		}       
     }
 }
